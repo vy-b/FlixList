9b20e769e7cac66bda3826578072b75df01f0c18
SheaJanke <sheajanke@gmail.com>
2021-01-24 22:22:33 -0800

    Setting up React project.

---
+++ project/src/App.js
@@ -0,0 +1,9 @@
+import TestComponent from "./Components/TestComponent"
+
+function App() {
+  return (
+    <TestComponent/>
+  );
+}
+
+export default App;
---
+++ project/src/Components/TestComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class TestComponent extends React.Component {
+    render(){
+        return <h1> This is a test!</h1>
+    }
+}
+
+export default TestComponent
