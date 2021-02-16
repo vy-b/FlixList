4a9e704307f71afe6823d8a6422ab15ef1f16fde
callumb <callumb@sfu.ca>
2021-02-16 15:45:05 -0800

    added logo and icons

--- project/src/Components/TopNavView.jsx
+++ project/src/Components/TopNavView.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import {Nav, Navbar, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Link} from 'react-router-dom'
+import {FaSearch, FaUser, FaUserFriends, FaUserPlus, FaUserMinus, FaList} from 'react-icons/fa';
+import FlixListLogo from '../Logos/Logo2.svg';
 
 class TopNavView extends React.Component {
     render() {
         return (
         <div>
             <Navbar fixed="top" bg="dark" variant="dark">
-                <Navbar.Brand as={Link} to="/SignUp">Logo</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/SignUp">
+                    <img src = {FlixListLogo} alt = "FlixList Logo" />
+                </Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link as={Link} to="/">Browse</Nav.Link>
                 </Nav>
                 
                 <Nav>
-                    <Nav.Link as={Link} to="/Search">Search</Nav.Link>
+                    <Nav.Link as={Link} to="/Search">
+                        <FaSearch className = "searchIcon" />
+                    </Nav.Link>
                     <Nav.Link href="#login">Login</Nav.Link>
                     <Button as={Link} to="/SignUp" variant="outline-light">Sign Up</Button>
                 </Nav>
