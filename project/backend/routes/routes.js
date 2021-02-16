0283e96f189fb381a990c44d3c7f64bf6c5b6228
SheaJanke <sheajanke@gmail.com>
2021-02-15 21:03:11 -0800

    Added Friends table to database, and created functions to put/get data from the Friends table.

--- project/backend/routes/routes.js
+++ project/backend/routes/routes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const signupTemplateCopy = require('../models/signup.js')
+const friendTableEntry = require('../models/FriendTableEntry');
+const axios = require('axios')
+const dotenv = require('dotenv');
+dotenv.config();
 
 router.post('/signup', (request, response) => {
     const signupUser = new signupTemplateCopy({
@@ -12,6 +16,41 @@ router.post('/signup', (request, response) => {
     }).catch( error => {
         response.json(error);
     });
+});
+
+router.post('/addFriend', (request, response) => {
+    const friendEntry = new friendTableEntry({
+        username: request.body.username,
+        friendUsername: request.body.friendUsername
+    })
+    friendEntry.save().then(data => {
+        response.json(data);
+    }).catch( error => {
+        response.json(error);
+    });
 })
+
+router.get('/getFriends', (req, res, next) => {
+    friendTableEntry.find({username: req.query.username}).exec().then(doc => {
+        res.json(doc)
+    }).catch( err => console.log(err));
+})
+
+router.get('/testAPI', function(req, res, next) {
+    const target = req.query.title;
+    axios.request({
+      method: 'GET',
+      url: 'https://movie-database-imdb-alternative.p.rapidapi.com/',
+      params: {s: target, page: '1', r: 'json'},
+      headers: {
+        'x-rapidapi-key': process.env.API_KEY,
+        'x-rapidapi-host': 'movie-database-imdb-alternative.p.rapidapi.com'
+      }
+    }).then(movie=> {
+    	res.json(movie.data.Search);
+    }).catch(function (error) {
+    	console.error(error);
+    });
+});
 
 module.exports = router;
