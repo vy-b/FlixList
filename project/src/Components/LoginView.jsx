226a9db61e9701f3cf9e306b998832f9894034a0
Furkan <fayhan@sfu.ca>
2021-02-17 13:12:25 -0500

    LoginView code

---
+++ project/src/Components/LoginView.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class LoginView extends React.Component{
+    render(){
+        return(
+            <div className = "App">
+            <header className = "App-Header">
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="username" placeholder="examplename123" />
+                    </Form.Group>
+
+
+                    <Form.Group>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type = "password" placeholder = "Password"/>
+                    </Form.Group>
+                </Form>
+                <Button type = "submit">Login</Button>
+            
+            </header>
+            </div>
+        );
+    }
+}
+export default LoginView
