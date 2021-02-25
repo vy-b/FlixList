3c5de58ccb259614930ce6f5c3d80c946e2de801
Vy Bui <kvy.bui@gmail.com>
2021-02-24 18:22:19 -0800

    movie tests updated

--- project/src/Components/Movie.jsx
+++ project/src/Components/Movie.jsx
@@ -6,7 +6,7 @@ class Movie extends React.Component {
        const {title, poster, year,plot,rated,runtime,genre,actors} = this.props;
        return (
         <div className="card">
-        <a class="card-block stretched-link text-decoration-none" href="#Review">
+        <a className="card-block stretched-link text-decoration-none" href="#Review">
             <div className="row no-gutters">
                 <div className="col-auto">
                 <div className="card-block px-2">
@@ -18,9 +18,9 @@ class Movie extends React.Component {
                         <h1 className="title">{title}</h1>
                         <h5 className="text-muted"><span className="year">{year}</span> | <span className="rated">{rated}</span></h5>
                         <p className="plot" >{plot}</p>
-                        <p id="other" className="cast"><span className="font-weight-bold">Cast: </span>{actors}</p>
-                        <p id="other"><span className="font-weight-bold">Runtime: </span>{runtime}</p>
-                        <p id="other"><span className="font-weight-bold">Genre: </span>{genre}</p>
+                        <p><span className="font-weight-bold">Cast: </span><span className="cast"><span id="other" >{actors}</span></span></p>
+                        <p><span className="font-weight-bold">Runtime: </span><span id="other" className="runtime">{runtime}</span></p>
+                        <p id="other" className="genre">{genre}</p>
                     </div>
                 </div>
             </div>
