0af6491a319013c2354576c47407669106ba662b
Furkan <fayhan@sfu.ca>
2021-02-28 23:36:16 -0500

    fixed version

--- project/src/Components/ProfileTab/ProfileTabView.jsx
+++ project/src/Components/ProfileTab/ProfileTabView.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Button, Form } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./ProfileTab.css";
 
 class ProfileTabView extends React.Component{
     render(){
-        console.log(this.props.username)
         return(
             <div>
                 <header className = "App-header">
