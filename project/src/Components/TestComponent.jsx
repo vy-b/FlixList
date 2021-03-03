4fd92128c8bd21bcb4d3963ed5ad8605a4111240
Vy Bui <kvy.bui@gmail.com>
2021-03-03 00:29:19 -0800

    test component currently contains userReviewController function

--- project/src/Components/TestComponent.jsx
+++ project/src/Components/TestComponent.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import UserReview from './UserReview.jsx'
+import UserReview from './UserReview'
+import {addRating} from '../Utils/Utils';
+import RatingTableEntry from '../Objects/RatingTableEntry.jsx';
 class TestComponent extends React.Component {
-    render(){
-        return( 
+    contructor(){
+
+    }
+    
+    sendReview=(userReview)=>{
+        const {rating,review} = userReview;
+        const ratingTableEntry = new RatingTableEntry('testID',this.props.username,rating,review);
+        console.log(addRating(ratingTableEntry));
+        console.log(ratingTableEntry);
+
+    }
+
+    render() {
+        
+        return (
             <div className="App">
                 <header className="App-header">
-                    <UserReview username={this.props.username} />
+                    <UserReview onReview={this.sendReview}/>
                 </header>
             </div>
         )
     }
-
 }
 
 export default TestComponent
