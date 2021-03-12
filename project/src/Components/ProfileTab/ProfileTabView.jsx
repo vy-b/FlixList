4b31ff106febad0bd6ddf8b8e789852ea3fb5684
Furkan <fayhan@sfu.ca>
2021-03-11 20:40:01 -0500

    reviews of the user in the profile page

--- project/src/Components/ProfileTab/ProfileTabView.jsx
+++ project/src/Components/ProfileTab/ProfileTabView.jsx
@@ -2,12 +2,30 @@ import React from 'react';
 import "./ProfileTab.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import UserSearchController from "./UserSearch/UserSearchController"
+import ReviewCard from '../ReviewCard.jsx'
+import {getRatings} from '../../Utils/Utils.jsx'
 
 class ProfileTabView extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { reviews: [] };
+    }
+    componentDidMount() {
+        const username = this.props.username;
+            getRatings(undefined, username).then((userReviews) => {
+                this.setState({reviews: userReviews});
+            });
+    }
+
     render(){
         return(
             <div>
                 <header className = "profile">
+                    <div className = "reviewcard">
+                            {this.state.reviews.map((review, i) => {
+                                if (i<10) return <ReviewCard review={review} key={i} />;
+                            })}
+                        </div>
                     <div className = "parent">
                         <div className = "name">{this.props.username}</div>
                         <div className = "friendSearch">
