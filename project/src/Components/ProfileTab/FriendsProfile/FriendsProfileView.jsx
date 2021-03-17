69aca42cd68f7eb08e03121f3b35ba6204ee7b59
Vy Bui <kvy.bui@gmail.com>
2021-03-16 18:28:05 -0700

    formatting

--- project/src/Components/ProfileTab/FriendsProfile/FriendsProfileView.jsx
+++ project/src/Components/ProfileTab/FriendsProfile/FriendsProfileView.jsx
@@ -12,36 +12,37 @@ class FriendsProfileView extends React.Component{
                 <header className = "profile">
                     <div className = "reviewcard">
                         {reviews.length === 0 
-                            ? <i style={{paddingTop:"90px"}}> {friendUsername} has not reviewed anything</i>
-                            : undefined
-                            }
+                        ? <i style={{paddingTop:"90px"}}> {friendUsername} has not reviewed anything</i>
+                        : undefined
+                        }
                         {reviews.map((review, i) => {
                             return i<10 ? <ReviewCard review={review} key={i} />:undefined;
                         })}
+
+                    </div>
+                    <div className = "parent">
+                        <div className = "name">
+                            {this.props.friendUsername}
                         </div>
-                        <div className = "parent">
-                            <div className = "name">
-                                {this.props.friendUsername}
-                            </div>
-                            <Button className="my-button" variant="outline-dark" style={{marginTop:"70px"}} onClick={this.props.clickHandler}>
-                                {isMyFriend === true
-                                ?<div>following</div> 
-                                : <div>follow</div> 
-                                }
-                                
-                            </Button>
-                            {users.length !== 0 
-                            ? <div className = "friends" style={{marginBottom:"10px",paddingTop:"10vh"}}>{friendUsername}'s Friends List</div>
-                            : <div className="font-italic" style={{marginTop:"10px"}}>{friendUsername} is not following anyone</div>
+                        <Button className="my-button" variant="outline-dark" style={{marginTop:"70px"}} onClick={this.props.clickHandler}>
+                            {isMyFriend === true
+                            ?<div>following</div> 
+                            : <div>follow</div> 
                             }
-                            <div className="listFriends">
-                            {
-                            users.map((searchUser, i) => {
-                                return <User username={searchUser} myUsername={myUsername} key={i}/>
-                            })
-                            }
-                            </div>
+                            
+                        </Button>
+                        {users.length !== 0 
+                        ? <div className = "friends" style={{marginBottom:"10px",paddingTop:"10vh"}}>{friendUsername}'s Friends List</div>
+                        : <div className="font-italic" style={{marginTop:"10px"}}>{friendUsername} is not following anyone</div>
+                        }
+                        <div className="listFriends">
+                        {
+                        users.map((searchUser, i) => {
+                            return <User username={searchUser} myUsername={myUsername} key={i}/>
+                        })
+                        }
                         </div>
+                    </div>
                </header>
             </div>
         )
